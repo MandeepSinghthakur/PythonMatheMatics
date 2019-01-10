a515c18cb1d09afdb56c9ca03d9c1eae4af86b75
Mandeep Singh <mandeep419.singh@gmail.com>
2019-01-10 11:09:15 -0800

    typos

--- calculas/functions.py
+++ calculas/functions.py
@@ -3,7 +3,7 @@
 
 # Domain and range of function
  # The Domain of a function is the set of input values that the independent variable can validly assume 
- # The output set of a fucntion is called the range
+ # The output set of a function is called the range
  # For Example the domain of the function f(x) = 1/x is all nonzero real and complex numbers 
  # because 1/0 isn't defined . The Range is formed by the set of values obtained by substituting 
  # each number in the domain into 1/x, so in this case it is also all nonzero real and complex numbers
@@ -12,7 +12,7 @@
  # x^2 , the domain is all +ve and -ve numbers, but the range is only the +ve numbers.
 
 
- #Common Mathemtical Functions
+ #Common Mathematical Functions
   # sin() , cos() trignometric functions.
   # Other Trignometric functions - tan() and the inverse
   # equivalents of these functions asin(), acos() and atan().
